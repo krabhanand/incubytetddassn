90e498b6adadb60987e083139bdc6f5c04732123
Kumar Abhishek Anand <kr.abh.anand@gmail.com>
2020-12-16 19:56:24 +0530

    test passed for using custom delimiters

--- testsassignment/src/main/java/incubyte/StringCalculator.java
+++ testsassignment/src/main/java/incubyte/StringCalculator.java
@@ -1,13 +1,40 @@
 package incubyte;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class StringCalculator{
+    
+    private Pattern pattern;
+    private Matcher matcher;
+    
+    StringCalculator(){
+
+    }
     public int add(String numbers){
         if(numbers.equals(""))
             return 0;
-        String[] nums = numbers.split(",|\n");
+        pattern = Pattern.compile("//");
+        matcher = pattern.matcher(numbers);
+        String delimiter = "";
+        if(matcher.find())
+        {
+            //separate delimiter line and array of numbers by \n as point of separation
+            String[] sepdelim = numbers.split("\n");
+            //get delimiter from line
+            delimiter = sepdelim[0].split("//")[1];
+            //put array of numbers in numbers string
+            numbers = sepdelim[1];
+        }
+        else{
+            delimiter = ",|\n";
+        }
+        String[] nums = numbers.split(delimiter);
         int sum = 0;
         for(int i = 0;i<nums.length;i++)
             sum += Integer.parseInt(nums[i]);
         return sum;
+    
     }
 }
