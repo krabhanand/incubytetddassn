e3ab607e0287d8c80748dea377652226255caeaa
Kumar Abhishek Anand <kr.abh.anand@gmail.com>
2020-12-17 17:33:59 +0530

    test passed for allowing multiple delimiters of all lengths

--- testsassignment/src/main/java/incubyte/StringCalculator.java
+++ testsassignment/src/main/java/incubyte/StringCalculator.java
@@ -15,50 +15,84 @@ public class StringCalculator{
     public int add(String numbers) throws Exception{
         // set call count, increase by 1 on each call
         setCallCount(getCallCount()+1);
+
+        //if string empty return 0
         if(numbers.equals(""))
             return 0;
+
+        //prepare pattern matcher variables to split, match and get delimiters    
         Pattern pattern;
         Matcher matcher;
         pattern = Pattern.compile("//");
         matcher = pattern.matcher(numbers);
         String delimiter = "";
+
+        //find if custom delimiters exist
         if(matcher.find())
         {
-            //separate delimiter line and array of numbers by \n as point of separation
+            //separate numbers list and line of delimiters
             String[] sepdelim = numbers.split("\n");
-            //get delimiter from line
-            delimiter = sepdelim[0].split("//")[1];
-           // System.out.println(delimiter);
-            pattern = Pattern.compile("\\[");
-            matcher = pattern.matcher(delimiter);
-            if(matcher.find())
-            {
-                delimiter = delimiter.split("(^\\[)")[1];
-                delimiter = delimiter.split("]")[0];
-            }
-            if(delimiter.length() > 1)
-            {
-                delimiter = "[\\"+delimiter.charAt(0)+"]+";
-            }
             
-            System.out.println(delimiter);
+            //get group of delimiters
+            String delimitergrp = sepdelim[0].split("//")[1];
+
+            //find if multiple custom delimiters exist or single of length > 1
+            pattern = Pattern.compile("\\[");
+            matcher = pattern.matcher(delimitergrp);
+
+            //find the delimiter
+            delimiter = findDelimiter(delimitergrp,matcher);
+            
+            
             //put array of numbers in numbers string
             numbers = sepdelim[1];
-            System.out.println(numbers);
         }
         else{
             delimiter = ",|\n";
         }
         String[] nums = numbers.split(delimiter);
+
+        //find the sum from list and return it
+        return findSumFromNumbers(nums);
+        
+    }
+
+
+    private String findDelimiter(String delimitergrp, Matcher matcher) {
+        String delimiter = "";
+
+        //find if custom delimiters exist
+        if(matcher.find())
+            {
+                String[] delimiters = delimitergrp.split("(\\[)");
+                StringBuilder makeDelimiter = new StringBuilder("");
+                for(int j = 0; j<delimiters.length; j++) {
+                    String delim = delimiters[j].split("(\\])")[0];
+                    if(!(delim.equals("")))
+                        {
+                            makeDelimiter.append("[\\\\"+delim.charAt(0)+"]+");
+                            if(j != delimiters.length-1)
+                                makeDelimiter.append("|");
+                        }
+               }
+               delimiter = makeDelimiter.toString();
+            }
+            else{
+                //if custom delimiters do not exist, delimitergrp will have the only delimiter 
+                delimiter = delimitergrp;
+            }
+        return delimiter;
+    }
+
+    private int findSumFromNumbers(String[] nums) {
         int sum = 0;
         for(int i = 0;i<nums.length;i++){
-            System.out.println(nums[i]);
             int temp = Integer.parseInt(nums[i]);
             if(temp < 0) {
                 StringBuilder numExepList = new StringBuilder("");
-                for(;i< nums.length; i++){
-                    if(Integer.parseInt(nums[i]) < 0)
-                        numExepList.append(nums[i]+" ");
+                for(int j = i;j< nums.length; j++){
+                    if(Integer.parseInt(nums[j]) < 0)
+                        numExepList.append(nums[j]+" ");
                 }
                 throw new NumberFormatException("negatives not allowed : "+numExepList.toString());
             }
@@ -66,10 +100,9 @@ public class StringCalculator{
                 sum += temp;
         }
         return sum;
-    
     }
 
-    public void setCallCount(int count){
+    public void setCallCount(int count) {
         this.callCount = count;
     }
 
